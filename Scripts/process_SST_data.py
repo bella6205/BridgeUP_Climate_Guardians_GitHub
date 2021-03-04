1f7a4a323d16e1e327b36b014da417e1292d8a3e
Brown Scholar <brownscholar@BridgeUP-Lap-307.lan1>
2021-03-04 18:14:24 -0500

    scripts were updated

---
+++ Scripts/process_SST_data.py
@@ -0,0 +1,4 @@
+import setup
+setup.dataPath
+if __name__ == "__main__":
+	print("Hello")
---
+++ Scripts/setup.py
@@ -0,0 +1,9 @@
+#import libraries
+import os
+#set path to the data folder
+dataPath = "/Users/brownscholars/Desktop/2020_BridgeUP_Internships/Data"
+if __name__ == "__main__":
+	pass
+else:
+	os.chdir(dataPath)
+	print("Current working directory: ", os.getcwd())
